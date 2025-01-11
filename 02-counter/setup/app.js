a180142a44cab6b9925e800d8e02466dfd75a0d0
MDNROnik <nimurrahnananik@gmail.com>
2025-01-11 17:31:29 +0600

    another implementation of this project

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,16 +1,46 @@
 let counter = 0;
 
+
+const btns = document.getElementsByClassName("btn");
+const count = document.getElementById("value");
+//console.log(btns)
+;
+for(let i=0;i<btns.length;i++){
+    //console.log(btns[i]);
+    btns[i].addEventListener('click', function(e){
+        
+        if(btns[i].classList[1]=="decrease"){
+            count.textContent = --counter;
+        }
+
+        if(btns[i].classList[1]=="reset"){
+            count.textContent = counter = 0;
+        }
+
+        if(btns[i].classList[1]=="increase"){
+            count.textContent = ++counter;
+        }
+
+    });
+}
+
+
+
+
+
+
+
 //decrease
-document.querySelector(".decrease").addEventListener('click',function(){
-    document.getElementById("value").textContent = counter = counter-1;
-});
+// document.querySelector(".decrease").addEventListener('click',function(){
+//     document.getElementById("value").textContent = counter = counter-1;
+// });
 
-//reset
-document.querySelector(".reset").addEventListener('click',function(){
-    document.getElementById("value").textContent = counter = 0;
-});
+// //reset
+// document.querySelector(".reset").addEventListener('click',function(){
+//     document.getElementById("value").textContent = counter = 0;
+// });
 
-//increase
-document.querySelector(".increase").addEventListener('click',function(){
-    document.getElementById("value").textContent = counter = counter+1;
-});
+// //increase
+// document.querySelector(".increase").addEventListener('click',function(){
+//     document.getElementById("value").textContent = counter = counter+1;
+// });
